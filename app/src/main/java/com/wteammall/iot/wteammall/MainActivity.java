1f8b492d7703165ab2c8e4a1c10e78312af6ca6a
hdiot <hd_iot@163.com>
2016-10-30 11:49:40 +0800

    添加分块1.优惠券模块 2.首页模块 3.商品模块 4.用户模块

---
+++ app/src/main/java/com/wteammall/iot/wteammall/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wteammall.iot.wteammall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 首页（主页）
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
