a46cf860f613217a6fad34c08127e96a810427c8
hdiot <hd_iot@163.com>
2016-10-30 11:50:52 +0800

    添加分块1.优惠券模块 2.首页模块 3.商品模块 4.用户模块

--- app/src/main/java/com/wteammall/iot/wteammall/SplashActivity.java
+++ app/src/main/java/com/wteammall/iot/wteammall/SplashActivity.java
@@ -1,13 +1,22 @@
 package com.wteammall.iot.wteammall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
+/**
+ * 启动界面（*闪屏页面）
+ * 可以在启动页面添加版本更新等活动
+ */
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        //进入首页
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
     }
 }
---
+++ app/src/main/java/com/wteammall/iot/wteammall/UserModule/PersonalCenterActivity.java
@@ -0,0 +1,15 @@
+package com.wteammall.iot.wteammall.UserModule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wteammall.iot.wteammall.R;
+
+public class PersonalCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_center);
+    }
+}
